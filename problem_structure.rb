db1dee8f37f028914d5acc66a3ed676f6e3a6452
Jhonny Moreira <xDeniedAccess@gmail.com>
2016-11-14 17:37:32 -0200

    Fixing typo in 'problem_structure' script

--- problem_structure.rb
+++ problem_structure.rb
@@ -16,8 +16,8 @@ files = project_langs.map do |lang|
   "#{project_name}/#{lang}-lang/#{filename}#{EXTENSIONS[lang.to_sym]}"
 end
 
-langs_directory = project_langs.map { |lang| "#{project_name}/#{lang}-lang" }
+langs_directories = project_langs.map { |lang| "#{project_name}/#{lang}-lang" }
 
 FileUtils.mkdir(project_name)
-langs_directory.each { |directory| FileUtils.mkdir(directory) }
+langs_directories.each { |directory| FileUtils.mkdir(directory) }
 files.each { |file| FileUtils.touch(file) }
